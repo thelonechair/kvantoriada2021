7baecdab67fd504c3a7e57c9cb6e5390451a4d23
thelonechair <89252832+thelonechair@users.noreply.github.com>
2021-08-21 18:42:38 +0700

    Update programm_for_motors.ino

--- programm_for_motors.ino
+++ programm_for_motors.ino
@@ -1,5 +1,4 @@
 // вводим константы
-
 #define M1 4 // первый мотор (1-ое направление движения)
 #define M2 6 // второй мотор (1-ое направление движения)
 #define DIRECTION1 9 //первый мотор (2-ое направление движения)
@@ -26,25 +25,27 @@ void setup()  {
 void loop() {
   val = digitalRead(BUTTON1); // переменная val считывает состояние кнопки 1
   val1 = digitalRead(BUTTON2); // переменная val1 считывает состояние кнопки 2
-  val2 = digitalRead(BUTTON3); переменная val2 считывает состояние кнопки 3
+  val2 = digitalRead(BUTTON3); //переменная val2 считывает состояние кнопки 3
   if (val == HIGH) { // создаем условие: если кнопка1 нажата, то моторы 1 и 2 едут в одном направлении - например - назад
+    analogWrite(M1, 200); // указываем мощность моторов. это значение отвечает за скорость перемещения платформы. мы можем менять его в зависимости от того какая скорость нам нужна. 
     digitalWrite(M1,LOW);
     digitalWrite(DIRECTION1, HIGH);
-    
+    analogWrite(M2, 200); // указываем мощность моторов. это значение отвечает за скорость перемещения платформы. мы можем менять его в зависимости от того какая скорость нам нужна.
     digitalWrite(M2, LOW);
     digitalWrite(DIRECTION2, HIGH);
   }
   if (val1 == HIGH) { // создаем условие : если кнопка2 нажата, то моторы 1 и 2 останавливаются
- 
-    digitalWrite(M1, LOW);
+  
+  digitalWrite(M1, LOW);
   digitalWrite(DIRECTION1, LOW);
   digitalWrite(M2, LOW);
   digitalWrite(DIRECTION2, LOW);  
    } 
    if (val2 == HIGH) { // создаем условие: если кнопка3 нажата, то моторы 1 и 2 едут в обратном направлении, т.е. - вперед
+    analogWrite(M1, 200);
     digitalWrite(M1, HIGH);
     digitalWrite(DIRECTION1, LOW);
-    
+    analogWrite(M2, 200);
     digitalWrite(M2, HIGH);
     digitalWrite(DIRECTION2, LOW);
    }                  // создаем условия, чтобы кнопки не приходилось зажимать для выполнения их программы.

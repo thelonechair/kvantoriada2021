853737901f2ecfe4549ef3ceb5f92fbe330608fe
thelonechair <89252832+thelonechair@users.noreply.github.com>
2021-10-31 22:22:28 +0700

    Update programm_for_sensorT.ino

--- programm_for_sensorT.ino
+++ programm_for_sensorT.ino
@@ -1,46 +1,71 @@
-#include <TroykaDHT.h> // подключаем библиотеку для датчика
-DHT dht(7, DHT11); //создаем объект для работы с датчиком
-void setup() {
-  Serial.begin(9600); // открываем монитор порта
-  dht.begin();  // инициализируем датчик
+#include <NewPing.h>
+#include <Wire.h>  // подключение библиотеки Wire
 
+#define TRIG1 11
+#define ECHO1 12
+#define TRIG2 8
+#define ECHO2 9
+#define TRIG3 6
+#define ECHO3 7
+#define MAX_DISTANCE 200 // Константа для определения максимального расстояния, которое мы будем считать корректным.
+
+NewPing sonar (TRIG1,ECHO1, MAX_DISTANCE);
+NewPing sonar2(TRIG2,ECHO2, MAX_DISTANCE);
+NewPing sonar3(TRIG3,ECHO3, MAX_DISTANCE);
+
+int ADXL345 = 0x53; // Адрес I2C датчика ADXL345
+float X_out, Y_out, Z_out;  // Выходы
+
+
+void setup() {
+  // Инициализируем взаимодействие по последовательному порту на скорости 9600
+  Serial.begin(9600);
+  Wire.begin(); // Инициализация библиотеки Wire 
+  // Установите ADXL345 в режим измерения
+  Wire.beginTransmission(ADXL345); // Начать общение с устройством 
+  Wire.write(0x2D); // работа с регистром  POWER_CTL  - 0x2D
+  // Включить измерение
+  Wire.write(8); // (8dec -> 0000 1000 двоичный) Бит D3 High для разрешения измерения
+  Wire.endTransmission();
+  delay(10);
 }
 
+
 void loop() {
-  dht.read();
-  // проверяем состояние данных
-  switch(dht.getState()) {
-    // если всё OK
-    case DHT_OK:
-      // выводим показания влажности и температуры
-      Serial.print("Temperature = ");
-      Serial.print(dht.getTemperatureC());
-      Serial.println(" C \t");
-      Serial.print("Temperature = ");
-      Serial.print(dht.getTemperatureK());
-      Serial.println(" K \t");
-      Serial.print("Temperature = ");
-      Serial.print(dht.getTemperatureF());
-      Serial.println(" F \t");
-      Serial.print("Humidity = ");
-      Serial.print(dht.getHumidity());
-      Serial.println(" %");
-      break;
-    // если произошла ошибка контрольной суммы
-    case DHT_ERROR_CHECKSUM:
-      Serial.println("Checksum error");
-      break;
-    // если превышено время ожидания, то выводим в монитор порта это:
-    case DHT_ERROR_TIMEOUT:
-      Serial.println("Time out error");
-      break;
-    // если данных нет, датчик не реагирует или отсутствует
-    case DHT_ERROR_NO_REPLY:
-      Serial.println("Sensor not connected");
-      break;
-  }
- 
-  // ждём две секунды и повторяем цикл
-  delay(2000);
+
+  // Стартовая задержка, необходимая для корректной работы.
+  delay(1000);
+
+  // Получаем значение от датчика расстояния и сохраняем его в переменную
+  unsigned int distance1 = sonar.ping_cm();
+  unsigned int distance2 = sonar2.ping_cm();
+  unsigned int distance3 = sonar3.ping_cm();
+  // Печатаем расстояние в мониторе порта
+  Serial.print("$");
+  Serial.print(distance1);
+  Serial.print(" ");
+  Serial.print(distance2);
+  Serial.print(" ");
+  Serial.print(distance3);
+  Serial.println(";");
+
+  // === Считать данные акселерометра === //
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x32); // Начать с регистра 0x32 (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(ADXL345, 6, true); // Чтение всех 6 регистров, значение каждой оси сохраняется в 2 регистрах
+  X_out = ( Wire.read()| Wire.read() << 8); // Значение по оси X
+  X_out = X_out; //Для диапазона + -2g нам нужно разделить необработанные значения на 256 в соответствии с datasheet
+  Y_out = ( Wire.read()| Wire.read() << 8); // Значение по оси Y
+  Y_out = Y_out;
+  Z_out = ( Wire.read()| Wire.read() << 8); // Значение по оси Z
+  Z_out = Z_out;
+  Serial.print("Xa= ");
+  Serial.print(X_out/256);
+  Serial.print("   Ya= ");
+  Serial.print(Y_out/256);
+  Serial.print("   Za= ");
+  Serial.println(Z_out/256);
+  delay(1000);
 
 }
